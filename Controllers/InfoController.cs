41d0d29e4c39fc06b6774078f5fde9ee1389e444
Tris <tris.phillips@gmail.com>
2026-09-13 17:17:08 +0000

    Refactoring

--- Controllers/InfoController.cs
+++ Controllers/InfoController.cs
@@ -1,6 +1,5 @@
-using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
-using System.IO;
+using Microsoft.AspNetCore.Mvc;
 
 namespace xteinkInfo.Controllers;
 
@@ -9,72 +8,66 @@ namespace xteinkInfo.Controllers;
 public class InfoController : ControllerBase
 {
 
-    public static List<Info> Infos { get; set; } = new List<Info>();
-    
-    public InfoController(ILogger<InfoController> logger)
-    {
-       
-    }
+	public static List<Info> Infos { get; set; } = new List<Info>();
+	
+	public InfoController(ILogger<InfoController> logger)
+	{
+	}
 
-    [HttpPost(Name = "Info")]
-    public async Task<Info> SendInfo([FromBody] Info infoRequest)
-    {
-        var currentInfo = infoRequest;
-        Infos.Add(currentInfo);
-        await SendInfoToDevice(Infos);
-        return currentInfo;
-    }
+	[HttpPost(Name = "Info")]
+	public async Task<Info> SendInfo([FromBody] Info infoRequest)
+	{
+		var currentInfo = infoRequest;
+		Infos.Add(currentInfo);
+		await SendInfoToDevice(Infos);
+		return currentInfo;
+	}
 
-    private static async Task RunProcessAndShowOutput(string fileName, string arguments)
-    {
-        var startInfo = new ProcessStartInfo
-        {
-            FileName = fileName,
-            Arguments = arguments,
-            RedirectStandardOutput = true,
-            RedirectStandardError = true,
-            UseShellExecute = false,
-            CreateNoWindow = true
-        };
+	private static async Task RunProcessAndShowOutput(string fileName, string arguments)
+	{
+		var startInfo = new ProcessStartInfo
+		{
+			FileName = fileName,
+			Arguments = arguments,
+			RedirectStandardOutput = true,
+			RedirectStandardError = true,
+			UseShellExecute = false,
+			CreateNoWindow = true
+		};
+		using var process = Process.Start(startInfo)
+			?? throw new InvalidOperationException($"Failed to start process: {fileName} {arguments}");
+		var stdoutTask = process.StandardOutput.ReadToEndAsync();
+		var stderrTask = process.StandardError.ReadToEndAsync();
+		await process.WaitForExitAsync();
+		var stdout = await stdoutTask;
+		var stderr = await stderrTask;
+		Console.WriteLine($"=> {fileName} {arguments}");
+		if (!string.IsNullOrWhiteSpace(stdout))
+		{
+			Console.WriteLine(stdout.TrimEnd());
+		}
+		if (!string.IsNullOrWhiteSpace(stderr))
+		{
+			Console.WriteLine(stderr.TrimEnd());
+		}
+		Console.WriteLine($"Exit code: {process.ExitCode}");
+	}
 
-        using var process = Process.Start(startInfo)
-            ?? throw new InvalidOperationException($"Failed to start process: {fileName} {arguments}");
-
-        var stdoutTask = process.StandardOutput.ReadToEndAsync();
-        var stderrTask = process.StandardError.ReadToEndAsync();
-
-        await process.WaitForExitAsync();
-
-        var stdout = await stdoutTask;
-        var stderr = await stderrTask;
-
-        Console.WriteLine($"=> {fileName} {arguments}");
-        if (!string.IsNullOrWhiteSpace(stdout))
-        {
-            Console.WriteLine(stdout.TrimEnd());
-        }
-        if (!string.IsNullOrWhiteSpace(stderr))
-        {
-            Console.WriteLine(stderr.TrimEnd());
-        }
-        Console.WriteLine($"Exit code: {process.ExitCode}");
-    }
-
-    private async Task SendInfoToDevice(List<Info> infos)
-    {
-        var host = Environment.GetEnvironmentVariable("XTEINK_HOST") ?? "192.168.4.22";
-        var html = "<html><head><title>Info</title></head><body>";
-        foreach (var info in infos)
-        {
-            html += $"<h1>{info.Section}</h1><p>{info.Content}</p>";
-        }
-        html += "</body></html>";
-        try { System.IO.File.Delete("/content/info.html"); } catch {}
-        await System.IO.File.WriteAllTextAsync("/content/info.html", html);
-        try { System.IO.File.Delete("/content/info.epub"); } catch {}
-        await RunProcessAndShowOutput("pandoc", "/content/info.html -o /content/info.epub --toc --no-check-certificate");
-        await RunProcessAndShowOutput("curl", $"-F \"path=info.epub\" http://{host}/delete");
-        await RunProcessAndShowOutput("curl", $"-F \"file=@/content/info.epub\" http://{host}/upload");
-    }
+	private async Task SendInfoToDevice(List<Info> infos)
+	{
+		var host = Environment.GetEnvironmentVariable("XTEINK_HOST") ?? "192.168.4.22";
+		var html = "<html><head><title>Info</title></head><body>";
+		foreach (var info in infos)
+		{
+			html += $"<h1>{info.Section}</h1><p>{info.Content}</p>";
+		}
+		html += "</body></html>";
+		try { System.IO.File.Delete("/content/info.html"); } catch {}
+		await System.IO.File.WriteAllTextAsync("/content/info.html", html);
+		try { System.IO.File.Delete("/content/info.epub"); } catch {}
+		await RunProcessAndShowOutput("pandoc", "/content/info.html -o /content/info.epub --toc --no-check-certificate");
+		await RunProcessAndShowOutput("curl", $"-F \"path=info.epub\" http://{host}/delete");
+		await RunProcessAndShowOutput("curl", $"-F \"file=@/content/info.epub\" http://{host}/upload");
+	}
 }
 
--- Program.cs
+++ Program.cs
@@ -1,34 +1,19 @@
-
 namespace xteinkInfo;
 
 public class Program
 {
-    public static void Main(string[] args)
-    {
-        var builder = WebApplication.CreateBuilder(args);
-
-        // Add services to the container.
-
-        builder.Services.AddControllers();
-        // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-        builder.Services.AddEndpointsApiExplorer();
-        builder.Services.AddSwaggerGen();
-
-        var app = builder.Build();
-
-        // Configure the HTTP request pipeline.
-       
-        app.UseSwagger();
-        app.UseSwaggerUI();
-        
-
-        app.UseHttpsRedirection();
-
-        app.UseAuthorization();
-
-
-        app.MapControllers();
-
-        app.Run();
-    }
+	public static void Main(string[] args)
+	{
+		var builder = WebApplication.CreateBuilder(args);
+		builder.Services.AddControllers();
+		builder.Services.AddEndpointsApiExplorer();
+		builder.Services.AddSwaggerGen();
+		var app = builder.Build();
+		app.UseSwagger();
+		app.UseSwaggerUI();
+		app.UseHttpsRedirection();
+		app.UseAuthorization();
+		app.MapControllers();
+		app.Run();
+	}
 }
